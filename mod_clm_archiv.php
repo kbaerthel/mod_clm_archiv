a41caa5851820083877d0ede1f86af52d9efc10e
Klaus Bärthel <kbaerthel@t-online.de>
2016-06-29 22:07:59 +0200

    # Zugriff auf Konfiguration CLM-Komponente

--- mod_clm_archiv.php
+++ mod_clm_archiv.php
@@ -3,7 +3,10 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 if(!defined("DS")){define('DS', DIRECTORY_SEPARATOR);} // fix for Joomla 3.2
-// angemeldet
+
+// Konfiguration wird benötigt
+require_once (JPATH_SITE . DIRECTORY_SEPARATOR . "components" . DIRECTORY_SEPARATOR . "com_clm" . DIRECTORY_SEPARATOR . "clm" . DIRECTORY_SEPARATOR . "index.php");
+
 require_once (dirname(__FILE__).DS.'helper.php');
 
 $link = modCLM_ArchivHelper::getLink($params);
